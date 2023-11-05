ec287c3a92540a29987afbee4c8b530d2b0e7168
Kurt Stutsman <kurt@mujido.com>
2023-11-05 14:08:27 -0600

    Basic hello world

---
+++ main/main.cpp
@@ -0,0 +1,14 @@
+//
+// @project     wifi_sdcard
+//
+// @author      Kurt Stutsman <kstutsman@gmail.com>
+//
+// @license     Apache-2.0
+//
+
+#include <cstdio>
+
+extern "C" void app_main()
+{
+    std::printf("Hello world\n");
+}
